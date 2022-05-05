5465bdb684672042ec1abd5cc16d88c42a530dd2
JoeHolownia <joeholownia7@gmail.com>
2022-05-05 13:01:02 +1000

    Overwrote property names in molecular entity, fixed inheritance issue

--- molecular_entity.py
+++ molecular_entity.py
@@ -8,12 +8,18 @@ class MolecularEntity(nx.Graph):
     a molecule as a graph. This can be parsed to numerous common string formats.
     """
 
+    @property
+    def atoms(self):
+        return self.nodes
+
+    @property
+    def bonds(self):
+        return self.edges
+
     def __init__(self):
 
         # init super class
-        super(nx.Graph, self).__init__(self)
-
-
+        super().__init__()
 
         self.dihedrals = {}  # but these would be of a conformer??
         self.conformers = {}  # ??
@@ -27,7 +33,7 @@ class Conformer(MolecularEntity):
     def __init__(self, **kwargs):
 
         # init super class
-        super(MolecularEntity, self).__init__(self)
+        super().__init__()
         self.attributes = {kwargs}
 
 
@@ -41,8 +47,40 @@ class Fragment(MolecularEntity):
         self.attributes = {kwargs}
 
 
+class Atom():
+
+    def __init__(self):
+        pass
+
+
+class Bond():
+
+    def __init__(self):
+        pass
+
+
 if __name__ == "__main__":
 
+    # testing
+
     molecule = MolecularEntity()
+    #molecule.add_edge()
+    molecule.add_node(1)
+    molecule.add_node(2)
     molecule.add_edge(2, 1)
+    print(molecule.atoms)
+    print(molecule.bonds)
+    print(molecule.nodes)
+    print(molecule.edges)
+
+    print("Atoms: ")
+    print(molecule[1])
+    print(molecule[2])
+
+    print("Bonds: ")
+    print(molecule[1][2])
     print(molecule[2][1])
+    e = [('a', 'b', 0.3), ('b', 'c', 0.9), ('a', 'c', 0.5), ('c', 'd', 1.2)]
+    molecule.add_weighted_edges_from(e)
+    print(nx.dijkstra_path(molecule, 'a', 'd'))
+
