01f61ea1b6d67039eb903187f1bcbd472f980ab9
Callum Macfarlane <CallumDMacfarlane@live.com>
2022-05-06 13:35:30 +1000

    Set up basic Inheritance Structure

--- molecular_entity.py
+++ molecular_entity.py
@@ -1,12 +1,27 @@
 import networkx as nx
 
 
-class MolecularEntity:
+class _2DChemicalObj:
     """
     Class representing a molecular entity, i.e. with a unique structural connectivity
     represented as a graph and stereo-isomeric form. This object contains a networkx graph to represent
     te . This can be parsed to numerous common string formats.
     """
+    def __init__(self, **kwargs):
+
+        if 'smiles' in kwargs:
+            # init using smils
+            pass
+        # iterate for all init methods
+
+
+        # init graph
+        self._graph = nx.Graph()
+
+        # conformers of this molecular entity
+        self.conformers = {}
+
+        # other properties
 
     @property
     def atoms(self):
@@ -24,20 +39,27 @@ class MolecularEntity:
     def graph(self, value):
         self._graph = value
 
-    conformers = dict
+    def add_atom(self, atom: Atom2D):
+        if not isinstance(atom, Atom2D): # not sure if we actually want to add atoms this way
+            # might make it  easier to enforce minimum information
+            raise TypeError('atom must be of type Atom2D')
 
+    def add_bond(self, bond: Bond):
+        if not isinstance(bond, bond):
+            raise TypeError('bond must be of type Bond')
+
+
+class _3DChemicalObj(_2DChemicalObj):
     def __init__(self):
+        super().__init__()
 
-        # init graph
-        self._graph = nx.Graph()
-
-        # conformers of this molecular entity
-        self.conformers = {}
-
-        # other properties
+    def add_atom(self, atom: Atom3D):
+        if not isinstance(atom, Atom3D): # not sure if we actually want to add atoms this way
+            # might make it  easier to enforce minimum information
+            raise TypeError('atom must be of type Atom2D')
 
 
-class Conformer(MolecularEntity):
+class Molecule_2D(_2DChemicalObj):
     """
     Represents an individual conformational state of a molecular entity.
     """
@@ -59,37 +81,49 @@ class Conformer(MolecularEntity):
     def writePDB(self):
         return
 
+class Molecule_3D(_3DChemicalObj, Molecule_2D):
+    def __init__(self):
+        super().__init__()
 
-class Fragment(MolecularEntity):
+class Fragment_2D(_2DChemicalObj):
     """
     Represents information about a fragment of the graph object. I.e. a collection of nodes/edges.
     """
 
     def __init__(self):
+        super().__init__()
         self.index = (None, None)
 
+class Fragment_3D(_3DChemicalObj,Fragment_2D):
+    def __init__(self):
+        super().__init__()
 
-class Atom:
+
+class _Atom:
     element = None
     valence = None
 
     def get(self):
         pass
 
+    def __getitem__(self, item):
+        # Will prbably need to set up this for networkx to properly interface
+        pass
 
 
-class Atom2D(Atom):
+
+class Atom2D(_Atom):
 
     def __init__(self):
         pass
 
 
-class Atom3D(Atom):
+class Atom3D(_Atom):
 
     def __init__(self):
         pass
 
-class Bond():
+class Bond:
 
     def __init__(self):
         pass
