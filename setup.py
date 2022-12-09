eed0a68a7949bf5c96d6fc51c23ad88501b80f89
Callum Macfarlane <CallumDMacfarlane@live.com>
2022-12-09 16:33:59 +1000

    setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+setup(name='chemsitry_data_structure',
+      version='0.0.1',
+      description='',
+      author='Josef Holownia, Callum Macfarlane',
+      author_email='j.holownia@uq.net.au, callum.macfarlane@uq.edu.au',
+      url='https://github.com/JoeHolownia/chemistry_data_structure/tree/main',
+      long_description='''simple data structure to represent a molecules, including relevant parsers to common file formats''',
+      install_requires=[
+          'networkx',
+          'matplotlib',
+            'numpy',
+            'pulp'
+      ]
+      )
