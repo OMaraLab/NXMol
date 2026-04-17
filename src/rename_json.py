f8528e0e99fa879e2571053d38fa57d9cf94c021
BSB47 <s4782492@student.uq.edu.au>
2026-04-17 15:16:35 +1000

    restructure to remove redundant files; the refactor dir is now its own repo

--- src/rename_json.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-for _, _, fn_list in os.walk("fbd"):
-    for fn in fn_list:
-        if fn.endswith(".json"):
-            a = json.load(open(f"fbd/{fn}", "r"))
-            id = a['id']
-            os.rename(f"fbd/{fn}", f"fbd/{id}.json")
